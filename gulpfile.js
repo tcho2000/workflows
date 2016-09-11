b067243ce864f5e87fade05cffd9696ac05ba9bc
Black Coyote <tcho2000@hotmail.com>
2016-09-11 12:40:00 -0400

    Added Static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,8 @@ var jsSources = [
 ];
 
 var sassSources = ['components/sass/style.css'];
-
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 gulp.task('coffee', function(){
   gulp.src(coffeeSources)
     .pipe(coffee({bare: true})
@@ -47,6 +48,8 @@ gulp.task ('watch', function(){
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function(){
@@ -56,4 +59,14 @@ gulp.task('connect', function(){
   });
 });
 
-gulp.task('default',['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function(){
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('default',['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
